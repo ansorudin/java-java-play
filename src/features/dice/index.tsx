d7033a44b64fd70642b699d78bbb58cc22a72c88
aisyahalghf <alghifariaisyahputri@gmail.com>
2024-05-13 20:05:26 +0700

    dice

--- src/features/dice/index.tsx
+++ src/features/dice/index.tsx
@@ -1,5 +1,4 @@
-import { StyleSheet, Dimensions } from 'react-native';
-import React, { useState, useRef } from 'react';
+import { useState } from 'react';
 import { Box, Text, Button, ButtonText, Image } from '@gluestack-ui/themed';
 import { FC } from 'react';
 
@@ -19,32 +18,66 @@ const diceImages = [
 export const Dice: FC<DiceProps> = () => {
   const [dices, setDices] = useState([0, 1]);
   const [total, setTotal] = useState<any>(null);
+  const [show, setShow] = useState<boolean>(false);
+
+  const rollerDice = () => {
+    setShow(true);
+  };
 
   const showRandomDice = () => {
     const dice1 = Math.floor(Math.random() * 5);
     const dice2 = Math.floor(Math.random() * 5);
     setDices([dice1, dice2]);
     setTotal(dice1 + 1 + dice2 + 1);
+    setShow(false);
   };
 
   return (
-    <Box
-      flex={1}
-      rounded="$md"
-      alignItems="center"
-      bgColor="black"
-      justifyContent="center"
-      onTouchEnd={showRandomDice}>
-      <Box flexDirection="row" justifyContent="space-evenly" w="$full" rounded="$md">
-        <Image height={120} w={120} source={diceImages[dices[0]]} alt="dice1" />
-        <Image height={120} w={120} source={diceImages[dices[1]]} alt="dice2" />
+    <Box flex={1} alignItems="center" justifyContent="center">
+      <Box
+        h={120}
+        flexDirection="row"
+        justifyContent="space-evenly"
+        w="$full"
+        rounded="$xs"
+        display={show ? 'flex' : 'none'}>
+        <Image height={120} w={120} source={require('./assets/dice-game.gif')} alt="dice2" />
+        <Image height={120} w={120} source={require('./assets/dice-game.gif')} alt="dice2" />
       </Box>
-      <Text size="2xl" bold color="white" marginVertical={30}>
+
+      <Box
+        h={120}
+        flexDirection="row"
+        justifyContent="space-evenly"
+        w="$full"
+        rounded="$xs"
+        display={show ? 'none' : 'flex'}>
+        <Image
+          height={100}
+          w={100}
+          source={diceImages[dices[0]]}
+          alt="dice1"
+          borderWidth={2}
+          rounded="$lg"
+        />
+        <Image
+          height={100}
+          w={100}
+          source={diceImages[dices[1]]}
+          alt="dice2"
+          borderWidth={2}
+          rounded="$lg"
+        />
+      </Box>
+      <Text size="2xl" bold marginVertical={30}>
         {total}
       </Text>
       <Box>
-        <Button action="secondary">
+        <Button action="secondary" onTouchEnd={rollerDice} display={show ? 'none' : 'flex'}>
           <ButtonText> Tap Roller Dice</ButtonText>
+        </Button>
+        <Button action="secondary" onTouchEnd={showRandomDice} display={show ? 'flex' : 'none'}>
+          <ButtonText> Show Random Dice</ButtonText>
         </Button>
       </Box>
     </Box>
