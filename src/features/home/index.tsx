4f40cc364ca3209db9623c7a38d90af79c252222
aisyahalghf <alghifariaisyahputri@gmail.com>
2024-07-23 13:14:55 +0700

    screen profile

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,10 +1,49 @@
 import { Box, Text } from '@gluestack-ui/themed';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import NfcManager, { NfcEvents, Ndef, NfcTech } from 'react-native-nfc-manager';
+import { Alert, Button, SafeAreaView } from 'react-native';
 
 export const Home = () => {
+  // const [supported, setSupported] = useState(true);
+  // const [enabled, setEnabled] = useState(false);
+  // const [tag, setTag] = useState(null);
+
+  // function App() {
+  //   useEffect(() => {
+  //     NfcManager.start();
+  //     NfcManager.setEventListenerEnabled(true);
+
+  //     return () => {
+  //       NfcManager.stop();
+  //       NfcManager.setEventListenerEnabled(false);
+  //     };
+  //   }, []);
+
+  //   const readNfc = async () => {
+  //     try {
+  //       await NfcManager.requestTechnology(NfcManager.NfcTech.Ndef);
+  //       const tags = await NfcManager.getTag();
+  //       console.log(tags);
+  //       Alert.alert('NFC Data', JSON.stringify(tag));
+  //       await NfcManager.setEventListenerEnabled(false);
+  //     } catch (e) {
+  //       console.warn(e);
+  //     } finally {
+  //       NfcManager.setEventListenerEnabled(false);
+  //       await NfcManager.setEventListenerEnabled(false);
+  //       await NfcManager.stop();
+  //     }
+  //   };
+
   return (
     <Box>
-      <Text>Home</Text>
+      <Text>halo</Text>
     </Box>
+    // <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //   <Text>NFC Supported: {supported ? 'Yes' : 'No'}</Text>
+    //   <Text>NFC Enabled: {enabled ? 'Yes' : 'No'}</Text>
+    //   {tag && <Text>Tag ID: {tag.id}</Text>}
+    //   <Button title="Read NFC" onPress={readNfc} />
+    // </SafeAreaView>
   );
 };
